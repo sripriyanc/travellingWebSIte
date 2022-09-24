0b0dae8ff697061e009487a9d37968f05e72398a
sri priyan <sripriyanc01@gmailcom>
2022-09-24 11:46:57 +0530

    my first files

---
+++ new_projec/script.js
@@ -0,0 +1,19 @@
+const mainPage = document.querySelector("main");
+const logPage = document.querySelectorAll("log");
+const logIn = document.querySelector("login");
+const form1 = document.querySelector("form1");
+const inputGroup = document.querySelector("input-group");
+const formControl = document.querySelector("form-control");
+var button= document.querySelector("button");
+const fullViwe = document.querySelector("fullViwe ");
+const head = document.querySelector("head")
+
+
+button.addEventListener('click', submit );
+function submit(){
+    // logPage.classList.add("hide");
+    // fullviwe.classList.remove("hide")
+    alert("hello")
+
+}
+
